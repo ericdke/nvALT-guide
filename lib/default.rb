d8a870473a2d1cbeaa52ea7c91f9555fd0c8393b
Eric Dejonckheere <galaakk@gmail.com>
2013-04-13 21:01:42 +0200

    init

---
+++ lib/default.rb
@@ -0,0 +1,6 @@
+# All files in the 'lib' directory will be loaded
+# before nanoc starts compiling.
+
+include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Blogging
